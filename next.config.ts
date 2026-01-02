8e108b6389891f0a53a038756084a08950b365f5
Jorge Peraza <jorge@monkeys.cloud>
2026-01-02 16:52:00 -0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'export',
+  images: { unoptimized: true }, // required for export if you use next/image
+}
 
-export default nextConfig;
+export default nextConfig
